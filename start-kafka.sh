e3e1484a5414952729c0c227ea6d20adfb1a213c
ravibalsuni <bala.ravi2691@gmail.com>
2021-05-19 12:04:29 +0530

    initial commit

---
+++ start-kafka.sh
@@ -0,0 +1,2 @@
+cd /home/ravi/kafka-installation/kafka_2.12-2.8.0/
+./bin/kafka-server-start.sh config/server.properties
---
+++ start-zookeeper.sh
@@ -0,0 +1,2 @@
+cd /home/ravi/kafka-installation/kafka_2.12-2.8.0/
+./bin/zookeeper-server-start.sh config/zookeeper.properties
